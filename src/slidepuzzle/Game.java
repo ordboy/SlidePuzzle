cfb5dd501b3ffe13537d948f254674e631176d99
ordboy <stevie.oliphant31@gmail.com>
2018-10-26 15:12:11 +0200

    oklara förslag satt statisk metod i class

--- src/slidepuzzle/Game.java
+++ src/slidepuzzle/Game.java
@@ -4,13 +4,29 @@
 package slidepuzzle;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  *
  * @author stevi
  */
 public class Game {
-    ArrayList<SlideButtons> buttonList = new ArrayList();
+    public ArrayList<SlideButtons> buttonList = new ArrayList();
+    SlideButtons sb;
+    public Game(){
+        for (int i = 0; i < 15; i++) {
+            buttonList.add(sb = new SlideButtons(i+"",i));
+            
+        }
+       
+    }
     
-    
+    public void shuffleBoard(){
+           Collections.shuffle(buttonList);
+       }
+    public boolean hasWon(){
+        //todo jämföra array
+       return true; 
+    }
+        
 }
--- src/slidepuzzle/SlidePuzzle.java
+++ src/slidepuzzle/SlidePuzzle.java
@@ -4,12 +4,22 @@
 
 package slidepuzzle;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class SlidePussle {
 
+public class SlidePuzzle extends JFrame {
+    public static void go(){
+    Game game = new Game();
+    JPanel pane = new JPanel();
     
+    
+    
+    
+    
+    }    
     public static void main(String[] args) {
-        
+        go();
     }
 
 }
