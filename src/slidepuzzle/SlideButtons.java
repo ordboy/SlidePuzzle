18484222f02e26678a52fb0ba505b19b05a05ac9
ordboy <stevie.oliphant31@gmail.com>
2018-10-26 10:31:16 +0200

    oklara förslag

--- src/slidepuzzle/SlideButtons.java
+++ src/slidepuzzle/SlideButtons.java
@@ -8,5 +8,10 @@ package slidepuzzle;
  * @author stevi
  */
 public class SlideButtons {
-    
+    String name;
+    int value;
+    SlideButtons(String name,int value){
+        this.name = name;
+        this.value = value;
+    }
 }
