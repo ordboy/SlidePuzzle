50f14ae930f1f546a9a6c53269da750b6010fa1c
ordboy <stevie.oliphant31@gmail.com>
2018-10-26 10:30:46 +0200

    oklara förslag

--- src/slidepuzzle/Game.java
+++ src/slidepuzzle/Game.java
@@ -3,10 +3,14 @@
  */
 package slidepuzzle;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author stevi
  */
 public class Game {
+    ArrayList<SlideButtons> buttonList = new ArrayList();
+    
     
 }
